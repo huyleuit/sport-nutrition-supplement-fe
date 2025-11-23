01eec1fe754e1a3f8595df54b68159c9a346f7ca
huyleuit <22520549@gm.uit.edu.vn>
2025-11-23 23:52:17 +0700

    fix: update next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "standalone", // <--- Dòng quan trọng nhất để Docker chạy được
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bizweb.dktcdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
